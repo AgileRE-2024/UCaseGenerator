4b7bc4b3170dbcd75e9075e0ab2e300f08fe8f11
dzakwanfio <fiodzakwan@gmail.com>
2024-11-21 14:11:50 +0700

    feature connection

--- webapp/usecase/models.py
+++ webapp/usecase/models.py
@@ -1,12 +1,13 @@
-from django.db import models
 from django.contrib.auth.models import User
+from django.db import models
+
 
 class History(models.Model):
     history_id = models.CharField(max_length=6, primary_key=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
-        return f"History {self.history_id} for User {self.user.username}"
+        return f"History {self.history_id} untuk User {self.user.username}"
 
 class UseCase(models.Model):
     use_case_id = models.CharField(max_length=10, primary_key=True)
@@ -15,9 +16,6 @@ class UseCase(models.Model):
     description = models.TextField()
     created_date = models.DateTimeField(auto_now_add=True)
 
-    # Uncomment and modify if you have Aktor and Fitur models defined
-    # aktor = models.ManyToManyField('Aktor', related_name='usecases')
-    # fitur = models.ManyToManyField('Fitur', related_name='usecases')
 
     def __str__(self):
         return self.nama
@@ -36,6 +34,7 @@ class UseCaseSpecification(models.Model):
     def __str__(self):
         return self.specification_name
 
+# Model hubungan Aktor dan Fitur
 class ActorFeature(models.Model):
     actor_name = models.CharField(max_length=255)
     feature_name = models.CharField(max_length=255)
@@ -43,15 +42,42 @@ class ActorFeature(models.Model):
     def __str__(self):
         return f"{self.actor_name} - {self.feature_name}"
 
-# Uncomment these classes if you want to define Aktor and Fitur models
-# class Aktor(models.Model):
-#     nama = models.CharField(max_length=100)
+# Model hubungan antar fitur dalam UseCase
+from django.db import models
 
-#     def __str__(self):
-#         return self.nama
 
-# class Fitur(models.Model):
-#     nama = models.CharField(max_length=100)
+class FeatureConnection(models.Model):
+    RELATION_TYPE_CHOICES = [
+        ('include', 'Include'),
+        ('extend', 'Extend'),
+        ('none', 'None'),
+    ]
 
-#     def __str__(self):
-#         return self.nama
+    use_case = models.ForeignKey(
+        UseCase, 
+        on_delete=models.CASCADE, 
+        related_name='feature_connections',
+        null=True,  
+        blank=True  
+    )
+    feature_start = models.CharField(max_length=255)
+    feature_end = models.CharField(max_length=255)
+    relation_type = models.CharField(
+        max_length=20,
+        choices=RELATION_TYPE_CHOICES,
+        default='none'
+    )
+
+    def __str__(self):
+        return f"{self.feature_start} -> {self.feature_end} ({self.relation_type})"
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'use_case': self.use_case.id if self.use_case else None,
+            'feature_start': self.feature_start,
+            'feature_end': self.feature_end,
+            'relation_type': self.relation_type,
+        }
+
+
